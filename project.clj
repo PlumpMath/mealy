a28a91154d899f86f4b92b53c359d9b83179666b
Chad Harrington <chad@deercreeklabs.com>
2014-09-23 21:05:12 -0600

    Remove vestigial components from project.clj

--- project.clj
+++ project.clj
@@ -2,13 +2,9 @@
   :description "State machine using core.async chans and running in a go block"
   :url "http://www.farbetter.com"
   :license "Eclipse Public License - v 1.0"
-  :plugins [[lein-expectations "0.0.7"]
-            [lein-release "1.0.5"]]
+  :plugins [[lein-release "1.0.5"]]
   :lein-release {:scm :git
                  :deploy-via :clojars}
-  :profiles
-  {:dev {:source-paths ["dev"]}}
   :dependencies
-  [[com.taoensso/timbre "3.3.1"]
-   [org.clojure/clojure "1.6.0"]
+  [[org.clojure/clojure "1.6.0"]
    [org.clojure/core.async "0.1.338.0-5c5012-alpha"]])
