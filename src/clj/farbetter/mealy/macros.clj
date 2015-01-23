c49bbc663cba12dad055bb1ff9c91ef815744e8e
Chad Harrington <chad@farbetter.com>
2015-01-22 17:50:16 -0700

    Attempt to fix throw-err problem in cljs. Not working.

--- src/clj/farbetter/mealy/macros.clj
+++ src/clj/farbetter/mealy/macros.clj
@@ -7,14 +7,15 @@
 
 (defmacro if-cljs
   "Return then if we are generating cljs code and else for Clojure code.
-   https://groups.google.com/d/msg/clojurescript/iBY5HaQda4A/w1lAQi9_AwsJ"
+  https://groups.google.com/d/msg/clojurescript/iBY5HaQda4A/w1lAQi9_AwsJ"
   [then else]
   (if (cljs-env? &env) then else))
 
-(defn throw-err [e]
-  (if (instance? Throwable e)
-    (throw e)
-    e))
+(defmacro throw-err [e]
+  `(let [e# ~e]
+     (if (instance? Throwable e#)
+       (throw e#)
+       e#)))
 
 (defmacro <!? [ch]
   `(if-cljs
