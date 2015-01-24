09449336ba5b42ba5d374c2c6b8173923294ce98
Chad Harrington <chad@farbetter.com>
2015-01-23 20:36:36 -0700

    Revert to old project.clj; prior changes were causing the jar not to be found by consumers. Strange.

--- project.clj
+++ project.clj
@@ -3,19 +3,10 @@
   :url "https://github.com/farbetter/mealy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :plugins [[com.cemerick/clojurescript.test "0.3.3"]
             [lein-cljsbuild "1.0.3"]
             [lein-release "1.0.6"]]
-
-  :deploy-repositories [["clojars" {:creds :gpg}]]
-  :lein-release {:scm :git
-                 :deploy-via :clojars}
-
-  :profiles {:dev
-             {:plugins [[com.keminglabs/cljx "0.5.0"]]
-              :repl-options
-              {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl
-                                  cljx.repl-middleware/wrap-cljx]}}}
 
   :dependencies
   [[com.taoensso/encore "1.19.1"]
@@ -24,8 +15,13 @@
    [org.clojure/clojurescript "0.0-2665"]
    [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
-  :repl-options
-  {:init-ns farbetter.mealy}
+  :profiles {:dev
+             {:plugins [[com.keminglabs/cljx "0.5.0"]]
+              :repl-options
+              {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl
+                                  cljx.repl-middleware/wrap-cljx]}}}
+
+  :prep-tasks [["cljx" "once"]]
 
   :cljx {:builds [{:source-paths ["src/cljx"]
                    :output-path "target/generated/src/clj"
@@ -42,26 +38,26 @@
 
   :cljsbuild
   {:builds
-   {"code-simple" {:source-paths ["target/generated/src/cljs"]
-                   :jar true
-                   :compiler
-                   {:output-to "target/simple/main.js"
-                    :output-dir "target/simple"
-                    :static-fns true
-                    :optimizations :simple}}
-    "test-simple" {:source-paths ["target/generated/src/cljs"
-                                  "target/generated/test/cljs"]
-                   :compiler
-                   {:output-to
-                    "target/test/simple/test_main.js"
-                    :output-dir "target/test/simple"
-                    :optimizations :simple}}}
+   {"code" {:source-paths ["target/generated/src/cljs"]
+            :compiler {:output-to "target/main.js"
+                       :optimizations :simple
+                       :pretty-print false
+                       :jar true}}
+    "test" {:source-paths ["target/generated/src/cljs"
+                           "target/generated/test/cljs"]
+            :compiler {:output-to "target/unit-test.js"
+                       :optimizations :simple
+                       :warnings true
+                       :pretty-print false}}}
+   :test-commands {"unit-tests" ["phantomjs" :runner
+                                 "target/unit-test.js"]}}
 
-   :test-commands
-   {"slimer-simple" ["slimerjs" :runner
-                     "target/test/simple/test_main.js"]
-    "phantom-simple" ["phantomjs" :runner
-                      "target/test/simple/test_main.js"]}}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
+  :lein-release {:scm {:name "git"
+                       :url "https://github.com/farbetter/mealy"}
+                 :deploy-via :clojars}
+
+
 
   :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
   :source-paths ["target/generated/src/clj" "src/clj"]
@@ -76,8 +72,8 @@
    "testcljs" ["do"
                "clean,"
                "cljx" "once,"
-               "cljsbuild" "once" "code-simple" "test-simple,"
-               "cljsbuild" "test" "slimer-simple"]
+               "cljsbuild" "once" "code" "test,"
+               "cljsbuild" "test" "unit-tests"]
    "testall" ["do"
               "testclj,"
               "testcljs"]})
