f3a9d97f0f87256d674b232cfd0b798b8c75ef4c
Chad Harrington <chad@farbetter.com>
2015-01-23 21:00:58 -0700

    lein-release plugin: bumped version from 0.7.10 to 0.7.11-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject farbetter/mealy "0.7.10"
+(defproject farbetter/mealy "0.7.11-SNAPSHOT"
   :description "State machine using core.async chans and running in a go block"
   :url "https://github.com/farbetter/mealy"
   :license {:name "Eclipse Public License"
